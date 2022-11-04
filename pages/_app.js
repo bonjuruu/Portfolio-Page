ba753281c92aec433f61c358281f5449d1619d72
bonjuruuu <sarah.kyounghui@gmail.com>
2022-11-04 17:20:20 +1300

    base profile page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+
+import 'tailwindcss/tailwind.css'
+import Navigation from "../components/Navigation";
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <ThemeProvider attribute="class" enableSystem={false}>
+    <Navigation/>
+    <Component {...pageProps} />
+    </ThemeProvider>
+    </>
 }
 
 export default MyApp
